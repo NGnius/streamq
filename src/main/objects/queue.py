229194c98d69f11bb21c59004219859db6a31a49
NGnius <ngniusness@gmail.com>
2019-06-06 21:48:10 -0400

    Add expected playback orders

--- src/main/objects/queue.py
+++ src/main/objects/queue.py
@@ -5,21 +5,33 @@ Created 2019-06-01 by NGnius
 from ..repo import gherkin
 from .sound import Sound
 from ..tools import codes
+import random
 
 class SoundQueue():
-    def __init__(self, id=None, sounds=list()):
+    def __init__(self, id=None, sounds=list(), shuffle=False, repeat=False, repeat_one=False, index=-1):
         self.version = 0
         self.id = id
         self.sounds = sounds
+        self.effective_queue = self.sounds
+        self.shuffle(shuffle=shuffle)
+        self.repeat = repeat
+        self.repeat_one = repeat_one
+        self.index = index
 
     def __add__(self, obj):
         if isinstance(obj, Sound):
+            if self._shuffle is True:
+                self.effective_queue.append(obj)
             self.sounds.append(obj)
         elif isinstance(obj, SoundQueue):
             for s in obj.sounds:
+                if self._shuffle is True:
+                    self.effective_queue.append(s)
                 self.sounds.append(s)
         else:
             raise TypeError("Only Sound and SoundQueue objects can be added to a SoundQueue")
+        # adding to a shuffle queue without an index should add to random spot
+        self.shuffle(shuffle=self._shuffle)
         return self
 
     def __eq__(self, other):
@@ -37,7 +49,7 @@ class SoundQueue():
         return str(self.sounds)
 
     def __getitem__(self, i):
-        return self.sounds[i]
+        return self.effective_queue[i]
 
     def add(self, obj, index=None):
         '''Like + operator, but index can be used to insert into a specific location'''
@@ -45,12 +57,50 @@ class SoundQueue():
             self += obj
         else:
             if isinstance(obj, Sound):
+                if self._shuffle is True:
+                    self.effective_queue.insert(index, obj)
                 self.sounds.insert(index, obj)
             elif isinstance(obj, SoundQueue):
                 for s in obj.sounds[::-1]:
+                    if self._shuffle is True:
+                        self.effective_queue.insert(index, s)
                     self.sounds.insert(index, s)
             else:
                 raise TypeError("Only Sound and SoundQueue objects can be added to a SoundQueue")
+
+    def shuffle(self, shuffle=True):
+        self._shuffle = shuffle
+        if self._shuffle is True:
+            upcoming_sounds = self.effective_queue[self.index:]
+            random.shuffle(upcoming_sounds)
+            self.effective_queue = self.effective_queue[:self.index] + upcoming_sounds
+        else:
+            self.effective_queue = list(self.sounds)
+
+    def now(self):
+        if (self.index < 0) or (self.index >= len(self.effective_queue)):
+            return
+        return self.effective_queue[self.index]
+
+    def next(self):
+        if self.repeat_one is True:
+            return self.now()
+        elif (self.index + 1) >= len(self.effective_queue):
+            if self.repeat is True:
+                self.index = -1
+            else:
+                return
+        self.index += 1
+        return self.now()
+
+    def previous(self):
+        if (self.index - 1) < 0:
+            if self.repeat is True:
+                self.index = len(self.effective_queue)
+            else:
+                return
+        self.index -= 1
+        return self.now()
 
     def to_jsonable(self):
         items = list()
--- src/test/tests/queue_test.py
+++ src/test/tests/queue_test.py
@@ -18,8 +18,8 @@ class SoundQueueTest(BaseCase):
     def test_queue_add(self):
         sound1 = queue.Sound()
         sound2 = queue.Sound()
-        self.queue1.sounds = [sound1]
-        self.queue2.sounds = [sound2]
+        self.queue1.sounds = self.queue1.effective_queue = [sound1]
+        self.queue2.sounds = self.queue2.effective_queue = [sound2]
         self.queue1 += self.queue2
         self.assertEqual(self.queue1.sounds, [sound1, sound2])
         self.assertEqual(self.queue2.sounds, [sound2])
@@ -30,7 +30,62 @@ class SoundQueueTest(BaseCase):
     def test_index(self):
         sound1 = queue.Sound()
         sound2 = queue.Sound()
-        self.queue1.sounds = [sound1, sound2]
+        self.queue1.sounds = self.queue1.effective_queue = [sound1, sound2]
 
         self.assertEqual(self.queue1[0], sound1)
         self.assertEqual(self.queue1[1], sound2)
+
+    def test_shuffle(self):
+        sound1 = queue.Sound()
+        sound2 = queue.Sound()
+        sound3 = queue.Sound()
+        self.queue1.sounds = self.queue1.effective_queue = [sound1, sound2, sound3]
+        self.queue1.shuffle()
+
+        self.assertTrue(self.queue1._shuffle)
+
+    def test_next(self):
+        sound1 = queue.Sound()
+        sound2 = queue.Sound()
+        self.queue1.sounds = self.queue1.effective_queue = [sound1, sound2]
+
+        self.assertEqual(self.queue1.next(), sound1)
+        self.assertEqual(self.queue1.next(), sound2)
+        self.assertIsNone(self.queue1.next())
+        self.assertEqual(self.queue1.index, len(self.queue1.effective_queue)-1)
+
+    def test_previous(self):
+        sound1 = queue.Sound()
+        sound2 = queue.Sound()
+        self.queue1.sounds = self.queue1.effective_queue = [sound1, sound2]
+
+        self.queue1.index = 2
+        self.assertEqual(self.queue1.previous(), sound2)
+        self.assertEqual(self.queue1.previous(), sound1)
+        self.assertIsNone(self.queue1.previous())
+        self.assertEqual(self.queue1.index, 0)
+
+    def test_repeat1(self):
+        sound1 = queue.Sound()
+        sound2 = queue.Sound()
+        self.queue1.sounds = self.queue1.effective_queue = [sound1, sound2]
+        self.queue1.index = 1
+        self.queue1.repeat_one = True
+
+        self.assertEqual(self.queue1.next(), sound2)
+        self.assertEqual(self.queue1.next(), sound2)
+        self.assertEqual(self.queue1.index, 1)
+
+    def test_repeatall(self):
+        sound1 = queue.Sound()
+        sound2 = queue.Sound()
+        self.queue1.sounds = self.queue1.effective_queue = [sound1, sound2]
+        self.queue1.index = 1
+        self.queue1.repeat = True
+
+        self.assertEqual(self.queue1.next(), sound1)
+        self.assertEqual(self.queue1.index, 0)
+        self.assertEqual(self.queue1.previous(), sound2)
+        self.assertEqual(self.queue1.index, 1)
+        self.assertEqual(self.queue1.next(), sound1)
+        self.assertEqual(self.queue1.next(), sound2)
